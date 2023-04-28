1083c16bd29e99d47b022a81d4a8c78b94c3f3c5
sepidehkavian <131254751+sepidehkavian@users.noreply.github.com>
2023-04-28 23:30:20 +0200

    Update sqrt_optimization.py

--- kavianpoor/hw3/sqrt_optimization.py
+++ kavianpoor/hw3/sqrt_optimization.py
@@ -1,23 +1,3 @@
-import cProfile
-import pstats
-import math
-from pstats import SortKey
-from template_hw1 import *
-import numpy as np
-
-'''def main():
-    with cProfile.Profile() as pr:
-        mysqrt(100)
-        pstats.Stats(pr).sort_stats(SortKey.TIME).print_stats()
-    
-    with cProfile.Profile() as pr:
-        np.sqrt(100)
-        pstats.Stats(pr).sort_stats(SortKey.TIME).print_stats()
-
-if __name__ == '__main__':
-    main()'''
-
-import timeit
 
 
 
@@ -70,70 +50,6 @@ if __name__ == '__main__':
 
 
 
-'''def fast_sqrt(x, n_steps=5):
-    if x < 0:
-        raise ValueError("Cannot compute square root of negative number")
-    if x == 0:
-        return 0
-    guess = x / 2
-    for i in range(n_steps):
-        guess = guess - ((guess ** 2 - x) / (2 * guess))
-    return guess'''
-
-
-
-
-
-
-
-
-
-'''def mysqrt_optimized(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-
-    x = n // 2
-    while True:
-        y = (x + n // x) // 2
-        if y >= x:
-            return x
-        x = y'''
-
-
-import cProfile
-import pstats
-from pstats import SortKey
-from template_hw1 import *
-import numpy as np
-
-'''def main():
-    inputs = {n: None for n in range(1, 10001)}
-
-    for n in inputs:
-        with cProfile.Profile() as pr:
-            mysqrt_optimized(n)
-        inputs[n] = pr
-
-    for n, stats in inputs.items():
-        print(f"n = {n}:")
-        pstats.Stats(stats).sort_stats(SortKey.TIME).print_stats()'''
-
-
-'''def main():
-    inputs = [1000000000000, 10, 1000, 100000]
-    for num in inputs:
-        with cProfile.Profile() as pr:
-            n= 1000
-            mysqrt(num)
-        stats = pstats.Stats(pr).sort_stats(SortKey.TIME)
-        stats.print_stats()
-        print("="*40)
-  
-
-if __name__ == '__main__':
-    main()'''
 
 
   
